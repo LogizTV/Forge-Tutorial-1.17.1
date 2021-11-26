6f67e4b5eee518aca929264e5ae73750c017a0a9
BuildTools <unconfigured@null.spigotmc.org>
2021-11-26 12:25:23 +0100

    parchments added

--- src/main/java/net/Loris/tutorialmod/TutorialMod.java
+++ src/main/java/net/Loris/tutorialmod/TutorialMod.java
@@ -1,33 +1,31 @@
 package net.Loris.tutorialmod;
 
-import net.minecraft.world.level.block.Block;
+import net.Loris.tutorialmod.item.Moditems;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(tutorialMod.MOD_iD)
-public class tutorialMod
+@Mod(TutorialMod.MOD_iD)
+public class TutorialMod
 {
     // Directly reference a log4j logger.
     public static final String MOD_iD = "tutorialmod";
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public tutorialMod() {
+    public TutorialMod() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        net.Loris.tutorialmod.item.Moditems.register(eventBus);
+        net.Loris.tutorialmod.block.ModBlocks.register(eventBus);
+
         eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
---
+++ src/main/java/net/Loris/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,41 @@
+package net.Loris.tutorialmod.block;
+
+import net.Loris.tutorialmod.item.Moditems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.Loris.tutorialmod.TutorialMod;
+import net.minecraftforge.fmllegacy.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> Blocks =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_iD);
+
+    public static final RegistryObject<Block> TITANIUM_BLOCK = registerBlock("titanium_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(12f)));
+
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = Blocks.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
+        Moditems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    }
+
+    public static void register(IEventBus eventBus) {
+        Blocks.register(eventBus);
+    }
+}
